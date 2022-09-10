aca7dd4f56e3f018352c4439ae6b7025daa8c435
Ishaan Khullar <ishaan.khullar06@gmail.com>
2022-09-10 21:40:10 +0530

    Completed Structure and designing!

--- lib/widgets/custom_balancebox.dart
+++ lib/widgets/custom_balancebox.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CustomBalanceBox extends StatefulWidget {
@@ -10,6 +12,107 @@ class CustomBalanceBox extends StatefulWidget {
 class _CustomBalanceBoxState extends State<CustomBalanceBox> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int amount = 10000;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Container(
+      height: height / 5,
+      width: width - 60,
+      margin: EdgeInsets.fromLTRB(30, 80, 20, 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Available Balance',
+              style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '₹ $amount',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w800),
+                ),
+                Image.asset(
+                  'assets/images/india.png',
+                  height: 40,
+                  width: 40,
+                ), // Im
+              ],
+            ),
+            SizedBox(
+              height: 26,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'See More',
+                      style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(Radius.circular(25))),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 12,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 6),
+                  child: Row(
+                    children: [
+                      Text(
+                        'INR',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Icon(
+                        Icons.arrow_drop_down_rounded,
+                        color: Colors.blue,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
